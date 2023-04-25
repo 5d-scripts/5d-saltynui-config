885425e919e7fe95954a654576ccd1734dd9b549
Ilias Rbayti <fivedscripts@gmail.com>
2023-04-25 15:06:05 +0200

    v1.0.0

---
+++ config.lua
@@ -0,0 +1,11 @@
+_CONFIG = 
+{
+    showUIOnMute = true,
+    toggleRangeCircle = 
+    {
+        enabled = true,
+        drawTimeSeconds = 2,
+        color = vector4(214, 167, 66, 165),
+        mutedColor = vector4(197, 26, 26, 165)
+    }
+}
