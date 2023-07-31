37c06605d6425470c6a619b5820391d30f68dcd6
Ilias Rbayti <128304168+5d-scripts@users.noreply.github.com>
2023-07-31 20:07:40 +0200

    v1.0.1
    - Fixed a bug where the script wouldnt wait for the UI to load before sending a NUI message, resulting in a delay display of the UI.
    
    - Added focusUI option to config.

--- config.lua
+++ config.lua
@@ -1,5 +1,7 @@
 _CONFIG = 
 {
+    focusUI = true, -- if set to true, the mouse cursor will be focused on the salty UI allowing you to click "copy link". because of that, the player is not able to move while the UI is open.
+                    -- if set to false, this feature becomes unavailable but results in the player still being able to move.
     showUIOnMute = true,
     toggleRangeCircle = 
     {
